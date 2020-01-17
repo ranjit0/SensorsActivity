b94a463d56f5796886f94654dbaaeea79eb00037
RANJIT <zodec.r1@gmail.com>
2020-01-17 07:27:23 +0545

    Initial commit

---
+++ app/src/main/java/com/ranjit/sensorsactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ranjit.sensorsactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
